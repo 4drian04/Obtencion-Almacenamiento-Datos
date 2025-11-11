27ee70534297f67094012751ae867e5f5681e798
4drian04 <adriacarlos2004@gmail.com>
2025-11-11 23:25:56 +0100

    Extracción de datos de la API de ESPN

--- actvidad1.7.py
+++ actvidad1.7.py
@@ -1,21 +1,24 @@
 import requests
+import db
 
 r = requests.get("https://site.web.api.espn.com/apis/v2/sports/soccer/esp.1/standings").json()
 
-estadisticas = ["gamesPlayed", "losses", "pointDifferential", "points", "pointsAgainst", "pointsFor", "ties", "rank"]
+estadisticas = ["gamesPlayed", "losses", "pointDifferential", "points", "pointsAgainst", "pointsFor", "ties", "rank", "wins"]
 liga = []
 equipos = {"equipos": {}}
 liga.append(r["abbreviation"])
 liga.append(r["children"][0]["abbreviation"])
 for equipo in r["children"][0]["standings"]["entries"]:
     nombreEquipo = equipo["team"]["name"]
-    equipos["equipos"][equipo["team"]["name"]] = {}
-    equipos["equipos"][equipo["team"]["name"]]["nombre"] = equipo["team"]["name"]
+    equipos["equipos"][nombreEquipo] = {}
+    equipos["equipos"][nombreEquipo]["nombre"] = equipo["team"]["name"]
     equipos["equipos"][nombreEquipo]["league"] = liga[0]
-    equipos["equipos"][equipo["team"]["name"]]["logo"] = equipo["team"]["logos"][0]["href"]
-    equipos["equipos"][equipo["team"]["name"]]["estadisticas"] = {}
+    equipos["equipos"][nombreEquipo]["logo"] = equipo["team"]["logos"][0]["href"]
+    equipos["equipos"][nombreEquipo]["estadisticas"] = {}
     for estadistica in equipo["stats"]:
         if estadistica["name"] in estadisticas:
-            equipos["equipos"][nombreEquipo]["estadisticas"][estadistica["name"]] = estadistica["value"]
+            equipos["equipos"][nombreEquipo]["estadisticas"][estadistica["name"]] = int(estadistica["value"])
 
-print(equipos)
+db.create_tables()
+db.insert_leagues(liga)
+db.insert_teams(list(equipos.values())[0])
